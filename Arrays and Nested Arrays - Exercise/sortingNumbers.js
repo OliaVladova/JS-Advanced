4362e165e619626d468f721e0fae4c17bb169090
OliaVladova <ovladova1@gmail.com>
2022-07-12 22:48:14 +0300

    Add files via upload

---
+++ Arrays and Nested Arrays - Exercise/sortingNumbers.js
@@ -0,0 +1,12 @@
+function sortArr(array){
+let sortedArr = array.sort((a,b)=> a - b);
+let newArr = [];
+while(sortedArr.length>0){
+    newArr.push(sortedArr.shift());
+    newArr.push(sortedArr.pop());
+}
+return newArr;
+
+}
+
+console.log(sortArr([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]));
